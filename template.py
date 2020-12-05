c08587bae8abd5ec646946c98be237aafcc05675
Kyle Graves <kyle@easypost.com>
2020-12-05 07:52:27 +0000

    adds template code file

---
+++ template.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+def main():
+    with open('2020/x/sample.txt', 'r') as f:
+        for line in f:
+            pass
+
+    part_one()
+    part_two()
+
+
+if __name__ == '__main__':
+    main()
