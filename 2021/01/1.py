ab848e5023e22d6507c1ff401ff3b3885742130f
Kyle Graves <kyle@easypost.com>
2021-12-01 07:49:15 +0000

    adds 2021 day 1 solution

---
+++ 2021/01/1.py
@@ -0,0 +1,58 @@
+import itertools
+import sys
+
+
+def iter_pairs(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def iter_triple(iterable):
+    a, b, c = itertools.tee(iterable, 3)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    return zip(a, b, c)
+
+
+def part_one(nums):
+    increases = 0
+    decs = 0
+    steps = 0
+    for prev, curr in iter_pairs(nums):
+        if curr > prev:
+            increases += 1
+
+    print(increases)
+
+
+def part_two(nums):
+    increases = 0
+    iter_ = iter_triple(nums)
+    prev, curr, next_ = next(iter_)
+    prev_sum = prev + curr + next_
+
+    for prev, curr, next_ in iter_:
+        curr_sum = (prev + curr + next_)
+        if prev_sum < curr_sum:
+            increases += 1
+
+        prev_sum = curr_sum
+
+    print(increases)
+
+
+def main():
+    lines = []
+
+    with open('2021/01/input.txt', 'r') as f:
+        for line in f:
+            lines.append(int(line.strip()))
+
+    part_one(lines)
+    part_two(lines)
+
+
+if __name__ == '__main__':
+    main()
