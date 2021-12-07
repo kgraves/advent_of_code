5e3e0720cecdac142d552da1a70dc20d4a7c037a
Kyle Graves <kyle@easypost.com>
2021-12-07 02:16:49 +0000

    adds 2021 day 3 part 2 solution

--- 2021/03/3.py
+++ 2021/03/3.py
@@ -22,10 +22,38 @@ def part_one(lines):
     print(gamma_int * epsilon_int)
 
 
-def part_two(lines):
-    # TODO
-    pass
+def narrow_candidates(candidates, index, keep_most_common=True):
+    ones_count = sum([int(c[index]) for c in candidates])
+    are_ones_most_common = ones_count >= (len(candidates) / 2)
 
+    most_common_digit = '1' if are_ones_most_common else '0'
+    least_common_digit = '0' if most_common_digit == '1' else '1'
+    num_looking_for = most_common_digit if keep_most_common else least_common_digit
+
+    return [c for c in candidates if c[index] == num_looking_for]
+
+
+def part_two(lines):
+    candidates = lines
+
+    o2_candidates = lines[:]
+    co2_candidates = lines[:]
+
+    while len(o2_candidates) > 1:
+        for i in range(len(o2_candidates[0])):
+            o2_candidates = narrow_candidates(o2_candidates, i)
+
+            if len(co2_candidates) == 1:
+                break
+
+    while len(co2_candidates) > 1:
+        for i in range(len(co2_candidates[0])):
+            co2_candidates = narrow_candidates(co2_candidates, i, keep_most_common=False)
+
+            if len(co2_candidates) == 1:
+                break
+
+    print(int(o2_candidates[0], 2) * int(co2_candidates[0], 2))
 
 def main():
     lines = []
@@ -35,7 +63,7 @@ def main():
             lines.append(line.strip())
 
     part_one(lines)
-    # part_two(lines)
+    part_two(lines)
 
 
 if __name__ == '__main__':
